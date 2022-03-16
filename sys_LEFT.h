63d421541162b996e120c7ef8b8ff9c47f6a9f3b
zjzhao <zjzhao@zjzhaodeair.speedport_w_724v_typ_a_05011603_06_003>
2022-03-16 14:20:15 +0100

    Add the operators corresponding to C9p and C10p

--- sys_LEFT.h
+++ sys_LEFT.h
@@ -24,8 +24,32 @@ struct sys_devlr {
   sys_devlr(double eqed_in, double ldevlr_2_in, double ldevlr_3_in, double ledvll_1_in, double ledvll_2_in, double ledvll_3_in): eqed(eqed_in), ldevlr_2(ldevlr_2_in), ldevlr_3(ldevlr_3_in), ledvll_1(ledvll_1_in), ledvll_2(ledvll_2_in), ledvll_3(ledvll_3_in) { }
 
   void operator() (const state_type &x, state_type &dxdt, const double t) {
-    dxdt[0] = (-8.0/3.0*eqed*eqed*x[0]+4.0/3.0*(ldevlr_2+ldevlr_3+ledvll_1+ledvll_2+ledvll_3))/(16.0*M_PI*M_PI);
+    dxdt[0] = (-8.0/3.0*eqed*eqed*x[0]+4.0/3.0*eqed*eqed*(ldevlr_2+ldevlr_3+ledvll_1+ledvll_2+ledvll_3))/(16.0*M_PI*M_PI);
   }
 };// L^{V,LR}_{de}
+
+struct sys_edvlr {
+  double eqed; 
+  double ledvlr_2, ledvlr_3;
+  double ledvrr_1, ledvrr_2, ledvrr_3;
+
+  sys_edvlr(double eqed_in, double ledvlr_2_in, double ledvlr_3_in, double ledvrr_1_in, double ledvrr_2_in, double ledvrr_3_in): eqed(eqed_in), ledvlr_2(ledvlr_2_in), ledvlr_3(ledvlr_3_in), ledvrr_1(ledvrr_1_in), ledvrr_2(ledvrr_2_in), ledvrr_3(ledvrr_3_in) { }
+
+  void operator() (const state_type &x, state_type &dxdt, const double t) {
+    dxdt[0] = (-8.0/3.0*eqed*eqed*x[0]+4.0/3.0*eqed*eqed*(ledvlr_2+ledvlr_3+ledvrr_1+ledvrr_2+ledvrr_3))/(16.0*M_PI*M_PI);
+  }
+};// L^{V,LR}_{ed}
+
+struct sys_edvrr {
+  double eqed;
+  double ledvrr_2, ledvrr_3;
+  double ledvlr_1, ledvlr_2, ledvlr_3;
+
+  sys_edvrr(double eqed_in, double ledvrr_2_in, double ledvrr_3_in, double ledvlr_1_in, double ledvlr_2_in, double ledvlr_3_in): eqed(eqed_in), ledvrr_2(ledvrr_2_in), ledvrr_3(ledvrr_3_in), ledvlr_1(ledvlr_1_in), ledvlr_2(ledvlr_2_in), ledvlr_3(ledvlr_3_in) { }
+
+  void operator() (const state_type &x, state_type &dxdt, const double t) {
+    dxdt[0] = (16.0/3.0*eqed*eqed*x[0]+4.0/3.0*eqed*eqed*(ledvrr_2+ledvrr_3+ledvlr_1+ledvlr_2+ledvlr_3))/(16.0*M_PI*M_PI);
+  }
+};// L^{V,RR}_{ed}
 
 #endif
--- sys_SMEFT.h
+++ sys_SMEFT.h
@@ -16,7 +16,7 @@ struct sys_lq1 {
   void operator() (const state_type &x, state_type &dxdt, const double t) {
     dxdt[0] = (-gp*gp*x[0]+(2.0/3.0)*gp*gp*(x[0]+clq1_2+clq1_3)+9.0*g*g*clq3-2.0/3.0*gp*gp*(-cqe_1-cqe_2-cqe_3)+0.5*Gu33*Gu33*x[0])/(16.0*M_PI*M_PI);
   }
-};//C^(1)_lq
+};//C^(1)_{lq}
 
 struct sys_lq3 {
   double g, gp;
@@ -29,7 +29,7 @@ struct sys_lq3 {
   void operator() (const state_type &x, state_type &dxdt, const double t) {
     dxdt[0] = (3.0*g*g*clq1-(6.0*g*g+gp*gp)*x[0]+2.0/3.0*g*g*(x[0]+clq3_2+clq3_3)+0.5*Gu33*Gu33*x[0])/(16.0*M_PI*M_PI);
  }
-};//C^(3)_lq
+};//C^(3)_{lq}
 
 struct sys_qe {
   double gp;
@@ -42,6 +42,30 @@ struct sys_qe {
   void operator() (const state_type &x, state_type &dxdt, const double t) {
     dxdt[0] = (4.0/3.0*gp*gp*(clq1_1+clq1_2+clq1_3+x[0]+cqe_2+cqe_3)+2.0*gp*gp*x[0]+0.5*Gu33*Gu33*x[0])/(16.0*M_PI*M_PI);
  }
-};//C_qe
+};//C_{qe}
+
+struct sys_ed {
+  double gp;
+  double ced_2, ced_3;
+  double cld_1, cld_2, cld_3;
+
+  sys_ed (double gp_in, double ced_2_in, double ced_3_in, double cld_1_in, double cld_2_in, double cld_3_in): gp(gp_in), ced_2(ced_2_in), ced_3(ced_3_in), cld_1(cld_1_in), cld_2(cld_2_in), cld_3(cld_3_in) { }
+
+  void operator() (const state_type &x, state_type &dxdt, const double t) {
+    dxdt[0] = (4.0*gp*gp*x[0]+4.0/3.0*gp*gp*(x[0]+ced_2+ced_3+cld_1+cld_2+cld_3))/(16.0*M_PI*M_PI);
+  }
+};//C_{ed}
+
+struct sys_ld {
+  double gp;
+  double cld_2, cld_3;
+  double ced_1, ced_2, ced_3;
+
+  sys_ld (double gp_in, double cld_2_in, double cld_3_in, double ced_1_in, double ced_2_in, double ced_3_in): gp(gp_in), cld_2(cld_2_in), cld_3(cld_3_in), ced_1(ced_1_in), ced_2(ced_2_in), ced_3(ced_3_in) { }
+
+  void operator() (const state_type &x, state_type &dxdt, const double t) {
+    dxdt[0] = (2.0/3.0*gp*gp*(ced_1+ced_2+ced_3+x[0]+cld_2+cld_3)-2.0*gp*gp*x[0])/(16.0*M_PI*M_PI);
+  }
+};//C_{ld}
 
 #endif
